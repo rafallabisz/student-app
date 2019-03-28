b6846d226e2ad6ed60b29f79021addfd6def89ca
Rafal Labisz <rafal0710@gmail.com>
2019-03-28 15:57:34 +0100

    Updates

---
+++ precache-manifest.b76c120e95386e982e871027d02309e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d19f3d83d3054ba42c44",
+    "url": "/student-app/static/js/runtime~main.0f0b220f.js"
+  },
+  {
+    "revision": "2dba962fd408b3abce8e",
+    "url": "/student-app/static/js/main.360d89d6.chunk.js"
+  },
+  {
+    "revision": "9354d7a4302890c131e2",
+    "url": "/student-app/static/js/2.c0f052af.chunk.js"
+  },
+  {
+    "revision": "2dba962fd408b3abce8e",
+    "url": "/student-app/static/css/main.915f6a53.chunk.css"
+  },
+  {
+    "revision": "a277e0a6e891052d9f6693ff5c90a285",
+    "url": "/student-app/index.html"
+  }
+];
